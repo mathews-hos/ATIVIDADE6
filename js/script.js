8f73d54274fde1b9bf01700d30a744bd057da2e1
mathews-hos <mathewshosoares@gmail.com>
2023-06-15 16:26:13 -0300

    ae

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function tema() {
+    const bori = document.body;
+    const tema = bori.getAttribute('data-bs-theme');
+    if (tema == 'dark') {
+        bori.setAttribute('data-bs-theme', 'light')
+    } else {
+        bori.setAttribute('data-bs-theme', 'dark')
+
+    }
+};
+function tabuada() {
+    const num = parseFloat(document.getElementById("tabuada").value)
+    const listaresul = document.getElementsByClassName("produto");
+    const listamultiplicar = document.getElementsByClassName("multiplicar")
+    if (num && num != 0) {
+        for (let i = 0; i < 10; i++) {
+            const multiplicar = listamultiplicar[i];
+            const produto = listaresul[i];
+            multiplicar.innerHTML = num
+            produto.innerHTML = num * (i + 1)
+        }
+    } else {
+        for (let i = 0; i < 10; i++) {
+            const multiplicar = listamultiplicar[i];
+            const produto = listaresul[i];
+            multiplicar.innerHTML = "-"
+            produto.innerHTML = "-"
+        }
+
+    }
+}
