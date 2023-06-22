74a3d05e18c7a914d37ae5175011cb299a500ac2
mathews-hos pc paula <mathewshosoares@gmail.com>
2023-06-21 23:12:54 -0300

    a

--- tarefa8.js
+++ tarefa8.js
@@ -1,8 +1,25 @@
+
+
+// 6) Utilize o método delete( ) para remover o 4º elemento do mapa.
+
+// 7) Utilize o método has( ) para verificar se existe algum aluno chamado "Jon Snow" neste mapa.
+
+// 8) Mostre todas as chaves do mapa utilizando o método keys( ).
+
+// 9) Mostre todos os valores do mapa utilizando o método values( ).
+
+// 10) Mostre as chaves e seus respectivos valores utilizando o método entries( ).
+
+
+
+
 const prompt = require('prompt-sync')();
 
-var alunos = ["Samuel", "Suellen", "André", "Brunno", "Carlos", "David", "Esther", "Gabriel", "Ilton", "João vitor", "Lucas", "Marcelo", "Mariana", "Mathews", "Rafael", "Ramon", "Ricardo"]
+const alunos = ["Samuel", "Suellen", "André", "Brunno", "Carlos", "David", "Esther", "Gabriel", "Ilton", "João vitor", "Lucas", "Marcelo", "Mariana", "Mathews", "Rafael", "Ramon", "Ricardo"]
 
 var notas = new Array
+
+// 1) A partir de um array de nomes e outro de notas, crie objetos da classe Aluno com os atributos "nome" e "nota" e adicione-os a um array de objetos. Utilize o loop FOR para percorrer os arrays.
 
 for (index = 0; index < alunos.length; index++) {
     notas.push((Math.random() * 10).toFixed(1))
@@ -10,27 +27,36 @@ for (index = 0; index < alunos.length; index++) {
 notas = notas.map(x => x > 9 ? 10 : parseFloat(x) + 1)
 // parseFloat(x) + 1
 
-const alunota = new Array
+const alunosComNota = new Array
 alunos.forEach((a, x) => {
     let aluno = {
         "nome": a,
         "nota": notas[x]
     }
-    alunota.push(aluno)
+    alunosComNota.push(aluno)
 })
-// console.log(alunota)
+// console.log(alunosComNota)
 
-alunota.forEach(x => console.log(x["nome"]))
+// 2) Percorra o array de objetos e exiba apenas o nome dos alunos.
 
-alunota.forEach(x => x["nota"] >= 7 ? x["situacao"] = "aprovado" : x["situacao"] = "reprovado")
+alunosComNota.forEach(x => console.log(x["nome"]))
 
+// 3) Percorra o array novamente para adicionar o atributo "situacao" a cada objeto. A situação do aluno será "Aprovado" se a nota for maior ou igual a 7.0 ou "Reprovado" caso contrário. Utilize o operador ternário na construção desta função.
 
-const mapalunos = new Map
+alunosComNota.forEach(x => x["nota"] >= 7 ? x["situacao"] = "aprovado" : x["situacao"] = "reprovado")
 
-alunota.forEach((item, itera) => {
-    mapalunos.set('aluno' + itera, item)
+// 4) Crie um mapa (objeto Map) a partir do seu array de objetos.
+
+const mapalunosENotas = new Map
+
+alunosComNota.forEach(aluno => {
+    mapalunosENotas.set(aluno['nome'], aluno["nota"])
 })
 
-console.table(alunota)
-mapalunos.forEach(x => { console.log(x); console.table(x) })
-console.log(mapalunos)
+console.table(alunosComNota)
+mapalunosENotas.forEach((x,y,)=> { console.log(x,y) })
+console.table(mapalunosENotas)
+
+// 5) Utilize o método get( ) para obter a nota do 5º elemento do mapa.
+quintoelemento = mapalunosENotas[alunos[4]]
+console.log(quintoelemento,alunos[4],mapalunosENotas[alunos[4]] )
